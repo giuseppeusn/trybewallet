11495794333906e4f8d04294f3ff3f1397bbd74e
Giuseppe <giuseppeuhlmann@gmail.com>
2022-05-19 14:27:31 -0300

    feat: adicionando requisito 7

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -85,7 +85,19 @@ class Wallet extends React.Component {
             Adicionar despesa
           </button>
         </div>
-        TrybeWallet
+        <table>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </table>
       </div>
     );
   }
